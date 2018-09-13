a7c220edbbba17891b136be3cad55ee25d03e511
zxzheng <35135685+zhengzhouxia@users.noreply.github.com>
2018-09-13 19:12:29 +0800

    Create step1.sh

---
+++ Arabidopsis_thaliana/step1.sh
@@ -0,0 +1,7 @@
+# RNAseq
+#download data
+nohup wget ftp://ftp.ensemblgenomes.org/pub/plants/release-28/fasta/arabidopsis_thaliana/cdna/Arabidopsis_thaliana.TAIR10.28.cdna.all.fa.gz &
+nohup wget ftp://ftp.ensemblgenomes.org/pub/plants/release-28/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.28.dna.genome.fa.gz &
+nohup wget  ftp://ftp.ensemblgenomes.org/pub/plants/release-28/gff3/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.28.gff3.gz &
+nohup wget ftp://ftp.ensemblgenomes.org/pub/plants/release-28/gtf/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.28.gtf.gz &
+#download rawdata
